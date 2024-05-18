88ec3115452b7fd126db80acce017117ff778c8b
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2024-05-18 22:32:53 +0300

    Remove feature/user_apps check because it was merged into main

--- api/dependency_check.py
+++ api/dependency_check.py
@@ -5,14 +5,6 @@ old_printfunc = print
 def print(*args,namespace:str="DependencyCheck",**kwargs):
 	old_printfunc(f"\033[2;34m\033[1;34m[{namespace}]\033[0m\033[2;34m\033[0m",*args,**kwargs)
 
-def check_discordpy_userapps():
-	try:
-		from discord import app_commands, AppCommandContext, AppInstallationType
-		testing = app_commands.Group(name='skidisherskidtru',description='skidisher skidtru dependency check',allowed_contexts=AppCommandContext.all(),allowed_installs=AppInstallationType.all())
-		return True
-	except Exception as ex:
-		return False
-
 def install_package_if_not_present(package_name:str,import_name:str=None):
 	"""
 	Installs a package using pip, if not present.
@@ -39,17 +31,7 @@ def dependency_check_auto():
 	Main function for Skidisher Skidtru to check dependencies.
 	"""
 
-	# https://www.squash.io/how-to-pip-install-from-a-git-repo-branch/
-	if check_discordpy_userapps() == False:
-		print(f"Skidisher Skidtru requires the feature/user_apps branch of discord.py",namespace="SkidisherSkidtru")
-		print(f"Uninstalling discord.py if present, then installing from git branch.",namespace="SkidisherSkidtru")
-		#print("https://github.com/rapptz/discord.py/tree/feature/user_apps",namespace="SkidisherSkidtru")
-		os.system(f"{sys.executable} -m pip uninstall discord")
-		install_package_if_not_present("git+https://github.com/rapptz/discord.py@feature/user_apps",'discord')
-		#exit(1)
-	else:
-		print("Discord package supports user apps")
-	install_package_if_not_present("git+https://github.com/rapptz/discord.py@feature/user_apps",'discord')
+	install_package_if_not_present("discord","discord") # discord.py PR #9760 got merged 
 	install_package_if_not_present("python-dotenv","dotenv")
 	install_package_if_not_present("cfscrape")
 	install_package_if_not_present("distro")

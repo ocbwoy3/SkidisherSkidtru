11365692e50740eb7522dbfa3cf15436a1f9dca1
OCbwoy3 <95974645+ocbwoy3@users.noreply.github.com>
2024-08-30 02:04:39 +0300

    wtf am i doing

--- plugins/Gayrate.py
+++ plugins/Gayrate.py
@@ -14,20 +14,18 @@ class Main:
 		@app_commands.command(name='gayrate',description='copy of heist\'s gayrate')
 		@app_commands.check(perms.is_whitelisted)
 		@app_commands.describe(user='gayrate user')
-		async def gayrate(interaction:Interaction,user:discord.User):
+		async def gayrate(interaction:Interaction,user:discord.User=None):
 			await interaction.response.defer(ephemeral=False,thinking=True)
+			if user == None: user = interaction.user
 			import random
 			gay = random.randint(0,10000)/100
-			if str(user.id) == "486147449703104523": gay = 10000
-			if str(user.id) == "1015304151133077505": gay = 10000
-			if str(user.id) == "479160987799257089": gay = 10000
+			if str(user.id) == "486147449703104523": gay = 100
+			if str(user.id) == "1015304151133077505": gay = 100
+			if str(user.id) == "479160987799257089": gay = 100
 			m = "😑"
-			if gay >= 25:
-				m = "😐"
-			if gay >= 50:
-				m = "🔥"
-			if gay >= 75:
-				m = "🏳️‍🌈"
+			if gay >= 25: m = "😐"
+			if gay >= 50: m = "🔥"
+			if gay >= 75: m = "🏳️‍🌈"
 			await interaction.followup.send(f"<@{user.id}> is {gay}% gay {m}")
 				
 		registrate.fun.add_command(gayrate)

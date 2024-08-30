25a2bc6178307f033774dd53f15bbdb4f0fc52d6
OCbwoy3 <95974645+ocbwoy3@users.noreply.github.com>
2024-08-30 03:41:51 +0300

    Update Gayrate.py

--- plugins/Gayrate.py
+++ plugins/Gayrate.py
@@ -8,6 +8,9 @@ import api.permissions as perms
 __plugin__ = "gayrate"
 __author__ = "OCbwoy3"
 
+global CACHE
+CACHE = {}
+
 class Main:
 	def __init__(self,registrate:CommandRegistrate):
 
@@ -16,12 +19,23 @@ class Main:
 		@app_commands.describe(user='gayrate user')
 		async def gayrate(interaction:Interaction,user:discord.User=None):
 			await interaction.response.defer(ephemeral=False,thinking=True)
+			global CACHE
 			if user == None: user = interaction.user
+			if str(user.id) == "1220838242270580737":
+				await interaction.followup.send("wtf?")
+				return
 			import random
 			gay = random.randint(0,10000)/100
 			if str(user.id) == "486147449703104523": gay = 100
 			if str(user.id) == "1015304151133077505": gay = 100
 			if str(user.id) == "479160987799257089": gay = 100
+			if str(user.id) == "458354633598042142": gay = 100
+			gay2 = CACHE.get(user.id,None)
+			if gay2 == None:
+				CACHE[user.id]=gay
+			else:
+				gay = gay2
+			
 			m = "😑"
 			if gay >= 25: m = "😐"
 			if gay >= 50: m = "🔥"
@@ -29,3 +43,12 @@ class Main:
 			await interaction.followup.send(f"<@{user.id}> is {gay}% gay {m}")
 				
 		registrate.fun.add_command(gayrate)
+		
+		@app_commands.command(name='gayrate_manipulate',description='copy of heist\'s gayrate')
+		@app_commands.check(perms.is_owner)
+		@app_commands.describe(user='gayrate user',gs='int')
+		async def manipulate(interaction:Interaction,user:discord.User,gs:int):
+			global CACHE
+			CACHE[user.id]=gs
+			await interaction.response.send(f"manipulated gs CACHE[{user.id}] = {gs}")
+		
